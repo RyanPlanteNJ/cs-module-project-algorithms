ee90f8dc70a110a75e2e3f1a6dc56f5d05df8b56
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-10-06 21:50:23 -0400

    finished moving zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -5,7 +5,15 @@ Returns: a List of integers
 def moving_zeroes(arr):
     # Your code here
 
-    pass
+    arr2= []
+
+    for i in range(0, len(arr)):
+        if arr[i] == 0:
+            arr2.append(i)
+    for i in reversed(range(len(arr2))):
+        arr.pop(arr2[i])
+        arr.append(0)
+    return arr
 
 
 if __name__ == '__main__':
